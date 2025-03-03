a81916d314365c460da705725251c99b333962ec
Hazem Al-Jammal <hazem.aljammal@mwamman.local>
2025-03-03 10:04:53 +0300

    devices-fcm-added

--- firebase/campaign.ts
+++ firebase/campaign.ts
@@ -1,4 +1,4 @@
-import { getFirestore, doc, setDoc } from 'firebase/firestore';
+import { getFirestore, doc, setDoc,getDoc} from 'firebase/firestore';
 
 export interface Campaign {
   id: string;
@@ -6,10 +6,22 @@ export interface Campaign {
   token?: string;
 }
 
-export async function storeCampaignToken(campaign: Campaign, token: string) {
+export async function storeCampaignToken(campaign: Campaign, token:string) {
+  storeDeviceFCM(token);
   const db = getFirestore();
   await setDoc(doc(db, 'campaigns', campaign.id), {
     ...campaign,
-    token
+    token,
+  });
+}
+
+export async function storeDeviceFCM(token: string) {
+  const db = getFirestore();
+  const tokenRef = doc(db, 'devices', token);
+  const checkToken = await getDoc(tokenRef);   if (checkToken.exists()) {
+    return;
+  }
+  await setDoc(doc(db, 'devices', token), {
+    token,
   });
 }
