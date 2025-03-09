73f42dd85e67fcd84ba647555c2801c0b95c8687
HazemAljammalMW <hazem.aljammal@mediaworldiq.com>
2025-03-09 15:46:03 +0300

    Update page.tsx

--- app/marketing/page.tsx
+++ app/marketing/page.tsx
@@ -4,12 +4,7 @@ import { isTokenSet } from "../api/auth/session";
 import { useRouter } from 'next/navigation';
 
 export default function Page() {
-    const router = useRouter();
-    const session = isTokenSet();
 
-    if (!session) {
-        router.push('/marketing');
-    }
     return (
         <div className="mt-10">
         <CampaignForm/>
